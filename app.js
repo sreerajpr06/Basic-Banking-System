e2eb8476b6c103b3014f8139b746c8d72412eaf3
sreerajpr06 <sreerajpr06@gmail.com>
2021-01-20 23:12:05 +0530

    added procfile, updated ports

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/bankDB", {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://admin-kingAtlan:atlantis@cluster0.zo5pc.mongodb.net/bankDB?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
 
 mongoose.set('useFindAndModify', false);
 
@@ -159,8 +159,12 @@ app.post('/processing', async function(req, res){
     res.redirect('/users');
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000");
+    console.log("Server started successfully");
 });
   
